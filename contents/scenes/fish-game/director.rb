fff4c5c6c73feadaa2adea429a406a462cda7c7c
森井大介 <e145758@moriidaisuke-no-MacBook-Pro.local>
2016-08-24 11:21:11 +0900

    change fish-game

---
+++ contents/scenes/fish-game/director.rb
@@ -0,0 +1,12 @@
+require_relative'bird'
+
+class Director
+  def initialize
+  	 @chars = []
+    2.times { @chars << Character.new(rand(100), -rand(400)+100, "contents/images/bird/dove-1.png") }
+@bg_img = Image_load("contents/images/fish/background-fish.png")
+end
+  	def play
+  		Window.draw(0, 0, @bg_img)
+end
+end
