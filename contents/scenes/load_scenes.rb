bc50ae7c2cc20fc7d047d1df1763b795b5d70f24
poster-keisuke <ta.keisuke4@gmail.com>
2016-08-24 11:08:12 +0900

    ファイルの整理

--- contents/scenes/load_scenes.rb
+++ contents/scenes/load_scenes.rb
@@ -1,10 +1,10 @@
 # require_relative 'scene_util'
 # require_relative 'title/director'
-# require_relative 'scene1/director'
+require_relative 'fishGame/director'
 # require_relative 'scene2/director'
 # require_relative 'ending/director'
 
 # Scene.add_scene(Title::Director.new,  :title)
-# Scene.add_scene(Scene1::Director.new,  :scene1)
+Scene.add_scene(FishGame::Director.new,  :fishgame)
 # Scene.add_scene(Scene2::Director.new,  :scene2)
 # Scene.add_scene(Ending::Director.new,  :ending)
