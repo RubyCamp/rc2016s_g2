d09d3b3566c4aa862cc904da3e36fffc4fd276c2
poster-keisuke <ta.keisuke4@gmail.com>
2016-08-24 14:42:09 +0900

    fishgameの背景画像の表示および、鳥の表示

--- contents/scenes/fishGame/director.rb
+++ contents/scenes/fishGame/director.rb
@@ -1,12 +1,15 @@
 require_relative'bird'
-
-class Director
-  def initialize
-  	 @chars = []
-  	 2.times { @chars << Character.new(rand(100), -rand(400)+100, "contents/images/bird/dove-1.png") }
-  	 @bg_img = Image_load("contents/images/fish/background-fish.png")
-  end
-  def play
-  	Window.draw(0, 0, @bg_img)
-  end
+module FishGame
+	class Director
+	  def initialize
+	  	 @chars = []
+	  	 2.times { @chars << Bird.new(rand(100), -rand(400)+100, "images/bird/dove-1.png") }
+	  	 @bg_img = Image.load("images/fish/background-fish.png")
+	  end
+	  def play
+	  	Window.draw(0, 0, @bg_img)
+	  	Sprite.update(@chars)
+	  	Sprite.draw(@chars)
+	  end
+	end
 end
