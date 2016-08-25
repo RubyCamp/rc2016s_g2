1111aeffdbfcf977ee4ecd7e2de34d9f823b4da8
Sawa Keisuke <sawa.window@outlook.jp>
2016-08-26 01:20:37 +0900

    Merge remote-tracking branch 'origin/staging' into staging
    # Conflicts:
    #	contents/scenes/birdGameBefore/director.rb
    #	contents/scenes/fishGameBefore/director.rb

--- contents/scenes/planktonGame/director.rb
+++ contents/scenes/planktonGame/director.rb
@@ -9,29 +9,48 @@ module PlanktonGame
       @chars = []
       @chars << Fishenemy.new(0, 0)
       @chars << Mo.new(700, rand(600))
-      @chars << Plankton.new
-      #@timer = 30 * 60
+  #    @chars << Plankton.new
+      @timer = 5 * 60 
       @repoptime = 100
       @repoptimer = @repoptime
       @bgm = Sound.new("sounds/gamePlaying.wav")
       @bgm_played = false
+      @plankton = Plankton.new
     end
 
     def play
       Window.draw(0, 0, @bg_img)
       Sprite.update(@chars)
+      Sprite.update(@plankton)
       Sprite.check(@chars, @chars)  #第一引数はあたる側、第二引数はあてられる側
+      Sprite.check(@chars,@plankton) 
       Sprite.draw(@chars)
+      Sprite.draw(@plankton)
       Sprite.clean(@chars)
+      Sprite.clean(@plankton)
+
       unless @bgm_played
         @bgm.play
         @bgm_played = true
       end
-      #@timer -= 1
+
+      @timer -= 1
+
      # if @timer
      #   Scene.set_current_scene(:scene1)
-     # end
+     # end 
+      
       @repoptimer -= 1
+       if @timer <= 0
+          @player_pos_logs = @plankton.y
+          p @player_pos_logs
+          @chars << Mo.new(700, @player_pos_logs )
+        end
+
+      if @timer == 0
+        @timer = 5*60
+      end
+
       if @repoptimer <= 0
         @repoptimer = @repoptime
         @repoptime -= 2
